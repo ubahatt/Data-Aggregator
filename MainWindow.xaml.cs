37be4f78643e9fd3ae55a2e8aced11e46db9dfcd
ubahatt <ubahatt@gmail.com>
2021-08-18 14:58:52 -0700

    Formatting changes

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -193,7 +193,7 @@ namespace DataAggregator
                     {
                         // More specific search term, but not all GUIs return us Time (sec), just Time will be more consistent
                         //tf = String.Equals(lineArray[t].Substring(0, 10), "Time (sec)"); // More
-                        tf =String.Equals(lineArray[t].Substring(0,4), "Time");
+                        tf = String.Equals(lineArray[t].Substring(0,4), "Time");
                         lineSkip++;
                         t++;
                     } while (tf == false);
